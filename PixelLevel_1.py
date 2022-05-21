40948f0bb1442b339a780565900d5ce7fe8271d5
Emma-GZ <58558662+Emma-GZ@users.noreply.github.com>
2022-05-21 20:19:27 +0800

    Update PixelLevel_1.py

--- PixelLevel_1.py
+++ PixelLevel_1.py
@@ -10,7 +10,6 @@ warnings.filterwarnings('ignore')  # ignore warnings
 height = 2 ** 10
 width = 2 ** 10
 
-
 def get_img(path):
     """
     Prepare an image for image processing tasks
@@ -21,29 +20,13 @@ def get_img(path):
     # resize function resize image to a soecific size; the type of returned numbers is float
     img = resize(img, (height, width), anti_aliasing=True, preserve_range=True)
 
-    # float → int
-    img = img.astype(int)
-
     return img
 
-def pixel_sim(path_a, path_b):
-    """
-  Measure the pixel-level similarity between two images
-
-  @returns:
-    percentage% that measures structural similarity between the input images
-  """
-    img_1 = get_img('D:\\aa.WKU\\WKU Course\\CPS3320-W01 PYTHON PROGRAMMING\\project\\photo\\test4.jpg')
-    img_2 = get_img('D:\\aa.WKU\\WKU Course\\CPS3320-W01 PYTHON PROGRAMMING\\project\\photo\\test.jpg')
-    return ( 1-np.sum(np.absolute(img_1 - img_2)) / (height * width) / 255)*100
-
-
 if __name__ == '__main__':
-    img_a = 'a.jpg'
-    img_b = 'b.jpg'
-    # get the similarity values
-    pixel_sim = pixel_sim(img_a, img_b)
-    print(str(pixel_sim)+"%")
+    img_1 = get_img('D:\\aa.WKU\\WKU Course\\CPS3320-W01 PYTHON PROGRAMMING\\project\\photo\\test3.jpg')
+    img_2 = get_img('D:\\aa.WKU\\WKU Course\\CPS3320-W01 PYTHON PROGRAMMING\\project\\photo\\test3_c.jpg')
+    pixel_sim=(1 - np.sum(np.absolute(img_1 - img_2)) / (height * width) / 255) * 100
+    print(str(pixel_sim) + "%")
 
 
 # 【Reference】https://gist.github.com/duhaime/211365edaddf7ff89c0a36d9f3f7956c
