d9a812cbacd76b4308314aaeb0df4322b6621b73
Emma-GZ <58558662+Emma-GZ@users.noreply.github.com>
2022-05-28 13:50:26 +0800

    Update Level1_PixelCopmarison.py

--- Level1_PixelCopmarison.py
+++ Level1_PixelCopmarison.py
@@ -18,8 +18,8 @@ def get_img(path):
     return img
 
 if __name__ == '__main__':
-    img_1 = get_img('D:\\aa.WKU\\WKU Course\\CPS3320-W01 PYTHON PROGRAMMING\\project\\photo\\test.jpg')
-    img_2 = get_img('D:\\aa.WKU\\WKU Course\\CPS3320-W01 PYTHON PROGRAMMING\\project\\photo\\test.jpg')
+    img_1 = get_img('image1.jpg')
+    img_2 = get_img('image2.jpg')
 
 if img_1.shape==img_2.shape:
     for i in range(img_1.shape[0]):
